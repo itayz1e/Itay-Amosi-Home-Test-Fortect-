8201cde9cda18b71a48a426125b360df39803882
itayz1e <itayz1e@gmail.com>
2024-06-24 21:06:23 +0300

    init commit

---
+++ electron-app/src/renderer/src/pages/Results.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+function Results() {
+  return (
+    <div>
+      <h1>Results page</h1>
+      <Link to="/">Home</Link>
+    </div>
+  )
+}
+
+export default Results
